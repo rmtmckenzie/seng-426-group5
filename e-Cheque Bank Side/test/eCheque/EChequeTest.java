5c9ed8eca9243a986a0c3ab61afe366043aa0717
Mateus Furquim <furquim.axs@gmail.com>
2014-07-05 17:19:49 -0700

    Creating ECheque Unit Test

---
+++ e-Cheque Bank Side/test/eCheque/EChequeTest.java
@@ -0,0 +1,240 @@
+/**
+ * 
+ */
+package eCheque;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author ubunteus
+ *
+ */
+public class EChequeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#ECheque()}.
+	 */
+	@Test
+	public void testECheque() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setaccountholder(java.lang.String)}.
+	 */
+	@Test
+	public void testSetaccountholder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setaccountNumber(java.lang.String)}.
+	 */
+	@Test
+	public void testSetaccountNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setbankname(java.lang.String)}.
+	 */
+	@Test
+	public void testSetbankname() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setpayToOrderOf(java.lang.String)}.
+	 */
+	@Test
+	public void testSetpayToOrderOf() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setamountofMony(java.lang.String)}.
+	 */
+	@Test
+	public void testSetamountofMony() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setcurrencytype(java.lang.String)}.
+	 */
+	@Test
+	public void testSetcurrencytype() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setchequeNumber(java.lang.String)}.
+	 */
+	@Test
+	public void testSetchequeNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setguaranteed(boolean)}.
+	 */
+	@Test
+	public void testSetguaranteed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setearnday(java.lang.String)}.
+	 */
+	@Test
+	public void testSetearnday() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setbanksignature(byte[])}.
+	 */
+	@Test
+	public void testSetbanksignature() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#setdrawersiganure(byte[])}.
+	 */
+	@Test
+	public void testSetdrawersiganure() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getMoney()}.
+	 */
+	@Test
+	public void testGetMoney() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getaccountholder()}.
+	 */
+	@Test
+	public void testGetaccountholder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getaccountNumber()}.
+	 */
+	@Test
+	public void testGetaccountNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getbankname()}.
+	 */
+	@Test
+	public void testGetbankname() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getpayToOrderOf()}.
+	 */
+	@Test
+	public void testGetpayToOrderOf() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getcurrencytype()}.
+	 */
+	@Test
+	public void testGetcurrencytype() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getchequeNumber()}.
+	 */
+	@Test
+	public void testGetchequeNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getguaranteed()}.
+	 */
+	@Test
+	public void testGetguaranteed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getearnday()}.
+	 */
+	@Test
+	public void testGetearnday() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getbanksignature()}.
+	 */
+	@Test
+	public void testGetbanksignature() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link eCheque.ECheque#getdrawersiganure()}.
+	 */
+	@Test
+	public void testGetdrawersiganure() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
