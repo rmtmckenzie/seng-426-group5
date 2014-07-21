ec8453b734772c17f19bee7745f2cd07d938b22a
Logan Bissonnette <lbiss@uvic.ca>
2014-07-21 12:07:29 -0700

    Fixed typos and formatting

--- e-Cheque Bank Side/src/eCheque/EChequeDB.java
+++ e-Cheque Bank Side/src/eCheque/EChequeDB.java
@@ -17,123 +17,115 @@ import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
 /**
- *
  * @author Saad
  */
 
 
 public class EChequeDB {
 
-    private static final String JDBC_DRIVER ="com.mysql.jdbc.Driver";
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     private static final String DATABASE_URL = "jdbc:mysql://localhost/ebank";
     private String userName;
     private String password;
     private Connection connection = null;
-    private Statement  sqlStatement = null;
+    private Statement sqlStatement = null;
     private ResultSet resultSet;
 
-    /** Creates a new instance of EChequeDB */
+    /**
+     * Creates a new instance of EChequeDB
+     */
     public EChequeDB() {
-        userName ="seng426";
-        password="log2IT05";
+        userName = "seng426";
+        password = "log2IT05";
     }
 
-    private boolean connectToDataBase()throws ClassNotFoundException, SQLException{
+    private boolean connectToDataBase() throws ClassNotFoundException, SQLException {
         // Initialize Connection to DB:
         Class.forName(JDBC_DRIVER); // load database driver class
         // establish connection to database
-        connection = DriverManager.getConnection( DATABASE_URL,userName,password);
+        connection = DriverManager.getConnection(DATABASE_URL, userName, password);
         return true;
     }
 
-    private boolean closeDataBaseConnection(){
+    private boolean closeDataBaseConnection() {
         try {
             // close the database connection channel
             connection.close();
             sqlStatement.close();
             //JOptionPane.showMessageDialog(null,"You are disconnected to e-Cheque Bank DB","DB State",JOptionPane.INFORMATION_MESSAGE);
             return true;
-        }
-        catch(SQLException exp){
+        } catch (SQLException exp) {
             //JOptionPane.showMessageDialog(null,exp.getMessage(),"DB Error",JOptionPane.ERROR_MESSAGE);
             exp.printStackTrace();
             return false;
         }
     }
 
-    private boolean createStatment()throws SQLException{
-
+    private boolean createStatement() throws SQLException {
         sqlStatement = connection.createStatement();
         return true;
     }
 
-    private void executeSQLStatment(String statement, int statType) throws SQLException{
+    private void executeSALStatement(String statement, int statType) throws SQLException {
         // Initialize sql statement and execute it.
-        if(statType == 0){
+        if (statType == 0) {
             resultSet = sqlStatement.executeQuery(statement);
-        }
-        else if(statType==1){
+        } else if (statType == 1) {
             sqlStatement.executeUpdate(statement);
         }
     }
 
-    public boolean runDB(int databaseMode, String databaseStat){
-        boolean flag= false;
-        try{
+    public boolean runDB(int databaseMode, String databaseStat) {
+        boolean flag = false;
+        try {
             connectToDataBase();
             //JOptionPane.showMessageDialog(null,"You are connected to e-Cheque Bank DB","DB State",JOptionPane.INFORMATION_MESSAGE);
-            createStatment();
+            createStatement();
             //JOptionPane.showMessageDialog(null,"You have created statment","DB State",JOptionPane.INFORMATION_MESSAGE);
-            executeSQLStatment(databaseStat, databaseMode);
+            executeSALStatement(databaseStat, databaseMode);
             flag = true;
-        }
-        catch(ClassNotFoundException exp){
+        } catch (ClassNotFoundException exp) {
             //JOptionPane.showMessageDialog(null,exp.getMessage(),"DB Error",JOptionPane.ERROR_MESSAGE);
             exp.printStackTrace();
-        }
-        catch(SQLException exp){
+        } catch (SQLException exp) {
             //JOptionPane.showMessageDialog(null,exp.getMessage(),"DB Error",JOptionPane.ERROR_MESSAGE);
             exp.printStackTrace();
-        }
-        finally {
+        } finally {
             closeDataBaseConnection();
         }
         return flag;
     }
 
-    public boolean runDB(int databaseMode, String databaseStat, double[]balance){
-        boolean flag= runDB(databaseStat,databaseMode);
+    public boolean runDB(int databaseMode, String databaseStat, double[] balance) {
+        boolean flag = runDB(databaseStat, databaseMode);
         try {
             balance[0] = flag ? resultSet.getDouble(1) : 0.0;
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null,e.getMessage(),"DB Error",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, e.getMessage(), "DB Error", JOptionPane.ERROR_MESSAGE);
             e.printStackTrace();
         }
         return flag;
     }
 
-    public boolean runDB(String databaseStat, int databaseMode){
-        boolean flag= false;
-        try{
+    public boolean runDB(String databaseStat, int databaseMode) {
+        boolean flag = false;
+        try {
             connectToDataBase();
-            JOptionPane.showMessageDialog(null,"You are connected to e-Cheque Bank DB","DB State",JOptionPane.INFORMATION_MESSAGE);
-            createStatment();
-            JOptionPane.showMessageDialog(null,"You have created statement","DB State",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(null, "You are connected to e-Cheque Bank DB", "DB State", JOptionPane.INFORMATION_MESSAGE);
+            createStatement();
+            JOptionPane.showMessageDialog(null, "You have created statement", "DB State", JOptionPane.INFORMATION_MESSAGE);
 
             // run the specific sql statement
-            executeSQLStatment(databaseStat,databaseMode);
+            executeSALStatement(databaseStat, databaseMode);
             flag = resultSet.next();
-        }
-        catch(ClassNotFoundException exp){
-            JOptionPane.showMessageDialog(null,exp.getMessage(),"DB Error",JOptionPane.ERROR_MESSAGE);
+        } catch (ClassNotFoundException exp) {
+            JOptionPane.showMessageDialog(null, exp.getMessage(), "DB Error", JOptionPane.ERROR_MESSAGE);
             exp.printStackTrace();
 
-        }
-        catch(SQLException exp){
-            JOptionPane.showMessageDialog(null,exp.getMessage(),"DB Error",JOptionPane.ERROR_MESSAGE);
+        } catch (SQLException exp) {
+            JOptionPane.showMessageDialog(null, exp.getMessage(), "DB Error", JOptionPane.ERROR_MESSAGE);
             exp.printStackTrace();
-        }
-        finally{
+        } finally {
             closeDataBaseConnection();
         }
         return flag;
