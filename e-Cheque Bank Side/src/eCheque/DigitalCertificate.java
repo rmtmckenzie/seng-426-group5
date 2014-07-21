90c92c44dcd766c46b67c109f4ab854e61e215db
Mac Button <demonzero14@gmail.com>
2014-07-21 12:29:40 -0700

    Refactored Digital certificate and Echeque classes

--- e-Cheque Bank Side/src/eCheque/DigitalCertificate.java
+++ e-Cheque Bank Side/src/eCheque/DigitalCertificate.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Abu^S3ooD
@@ -23,85 +22,84 @@ import java.io.*;
 import java.security.*;
 // this class generates Digital Signature
 
-public class DigitalCertificate implements Serializable
-{
-  
-  private String HolderName;
-  private String Subject ;
-  private String Issuer ;
-  private String SerialNumber;
-  private String ValidFrom;
-  private String ValidTo;
-  private PublicKey publicKey;
-  private byte[] IssuerSignature;
-    
-    /** Creates a new instance of certificate */
+public class DigitalCertificate implements Serializable {
+
+    private String HolderName;
+    private String Subject;
+    private String Issuer;
+    private String SerialNumber;
+    private String ValidFrom;
+    private String ValidTo;
+    private PublicKey publicKey;
+    private byte[] IssuerSignature;
+
+    /**
+     * Creates a new instance of certificate
+     */
     public DigitalCertificate() {
     }
-    public void setHolderName(String x)
-    {
-       HolderName= x;
+
+    public void setHolderName(String x) {
+        HolderName = x;
     }
-    public void setSubject(String x)
-    {
-        Subject= x;
+
+    public void setSubject(String x) {
+        Subject = x;
     }
-    public void setIssuer(String x)
-    {
-        Issuer= x;
+
+    public void setIssuer(String x) {
+        Issuer = x;
     }
-    public void setSerialNumber(String x)
-    {
-        SerialNumber= x;
+
+    public void setSerialNumber(String x) {
+        SerialNumber = x;
     }
-    public void setValidFrom (String x)
-    {
-        ValidFrom= x;
+
+    public void setValidFrom(String x) {
+        ValidFrom = x;
     }
-    public void setValidTo (String x)
-    {
-        ValidTo= x;
+
+    public void setValidTo(String x) {
+        ValidTo = x;
     }
-    public void setPublicKey (PublicKey x)
-    {
-        publicKey= x;
+
+    public void setPublicKey(PublicKey x) {
+        publicKey = x;
     }
-    public void setIssuerSignature (byte [] x)
-    {
-        IssuerSignature= x;
+
+    public void setIssuerSignature(byte[] x) {
+        IssuerSignature = x;
     }
-    
-    public String getHolderName()
-    {
-        return HolderName ;
+
+    public String getHolderName() {
+        return HolderName;
     }
-     public String getSubject()
-    {
-        return Subject ;
+
+    public String getSubject() {
+        return Subject;
     }
-      public String getIssuer()
-    {
+
+    public String getIssuer() {
         return Issuer;
     }
-       public String getSerialNumber()
-    {
+
+    public String getSerialNumber() {
         return SerialNumber;
     }
-        public String getValidFrom()
-    {
+
+    public String getValidFrom() {
         return ValidFrom;
     }
-    public String getValidTo()
-    {
+
+    public String getValidTo() {
         return ValidTo;
     }
-    public PublicKey getpublicKey()
-    {
+
+    public PublicKey getpublicKey() {
         return publicKey;
     }
-    
-         public byte[] getIssuerSignature()
-    {
+
+    public byte[] getIssuerSignature() {
         return IssuerSignature;
     }
 
@@ -118,5 +116,5 @@ public class DigitalCertificate implements Serializable
         out.writeObject(this);
         out.close();
     }
-     
+
 }
--- e-Cheque Bank Side/src/eCheque/ECheque.java
+++ e-Cheque Bank Side/src/eCheque/ECheque.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Basel
@@ -22,145 +21,121 @@ import java.io.Serializable;
 import java.io.*;
 
 //this class  for inter data cheque form user
-public class ECheque implements Serializable
-{
-     private String accountholder;
-     private String accountNumber;
-     private String bankname;
-     private String payToOrderOf ;
-     private String amountofMony;
-     private String currencytype;
-     private String chequeNumber;
-     private boolean guaranteed;
-     private String earnday;
-     private byte[] banksignature;
-     private byte[] drawersiganure;
-     
-     
-     
-     
-     
-     //to ener data we use set function 
-    /** Creates a new instance of ECheque */
+public class ECheque implements Serializable {
+
+    private String accountholder;
+    private String accountNumber;
+    private String bankname;
+    private String payToOrderOf;
+    private String amountofMoney;
+    private String currencytype;
+    private String chequeNumber;
+    private boolean guaranteed;
+    private String earnday;
+    private byte[] bankSignature;
+    private byte[] drawerSignature;
+
+    /**
+     * Creates a new instance of ECheque
+     */
     public ECheque() {
-        
-    }
-    
-    public void setaccountholder(String x)
-    {
-        accountholder = x;
-     
-    }
-    public void setaccountNumber(String y)
-    {
-        accountNumber=y;
-    }
-    
-    
-    public void setbankname(String z)
-    {
-        bankname=z;
-        
-    }
-  
-    public void setpayToOrderOf(String m)
-    {
-        payToOrderOf=m;
-        
-    }
-    
-    public void setamountofMony(String s){
-        amountofMony = s;
-    }
-    
-    public void setcurrencytype(String n)
-    {
-        currencytype=n;
-        
-    }
-    public void setchequeNumber(String c)
-    {
-        chequeNumber=c;
-    }
-    public void setguaranteed(boolean s)
-    {
-        guaranteed=s;
-        
-    }
-     public void setearnday(String u)
-    {
-        earnday= u;
-     
+
     }
 
-    
-    public void setbanksignature(byte[] y)
-    {
-        banksignature = y;
-     
-    } 
-   
-     public void setdrawersiganure(byte[] y)
-    {
-        drawersiganure = y;
-     
-    } 
-      
-     
-     
-     
-     //to extracting data we use get function;
-    public String getMoney()
-    {
-        return amountofMony;
+    public void setaccountholder(String x) {
+        accountholder = x;
+
     }
-    public String getaccountholder()
-    {
+
+    public void setaccountNumber(String y) {
+        accountNumber = y;
+    }
+
+    public void setbankname(String z) {
+        bankname = z;
+
+    }
+
+    public void setpayToOrderOf(String m) {
+        payToOrderOf = m;
+
+    }
+
+    public void setamountofMony(String s) {
+        amountofMoney = s;
+    }
+
+    public void setcurrencytype(String n) {
+        currencytype = n;
+
+    }
+
+    public void setchequeNumber(String c) {
+        chequeNumber = c;
+    }
+
+    public void setguaranteed(boolean s) {
+        guaranteed = s;
+
+    }
+
+    public void setearnday(String u) {
+        earnday = u;
+
+    }
+
+    public void setbanksignature(byte[] y) {
+        bankSignature = y;
+
+    }
+
+    public void setdrawersiganure(byte[] y) {
+        drawerSignature = y;
+
+    }
+
+    public String getMoney() {
+        return amountofMoney;
+    }
+
+    public String getaccountholder() {
         return accountholder;
     }
-    public String getaccountNumber()
-    {
-       return accountNumber;
+
+    public String getaccountNumber() {
+        return accountNumber;
     }
-    public String getbankname()
-    {
-         return bankname ;  
+
+    public String getbankname() {
+        return bankname;
     }
-        
-    public String getpayToOrderOf()
-    {
-       return  payToOrderOf;   
+
+    public String getpayToOrderOf() {
+        return payToOrderOf;
     }
-    
-    public String getcurrencytype()
-    {
-        return currencytype;   
+
+    public String getcurrencytype() {
+        return currencytype;
     }
-    
-    public String getchequeNumber()
-    {
+
+    public String getchequeNumber() {
         return chequeNumber;
     }
-    
-  
-    public boolean getguaranteed()
-    {
-        
-        return guaranteed ;
+
+    public boolean getguaranteed() {
+        return guaranteed;
     }
-        public String getearnday()
-    {
-        return  earnday;
+
+    public String getearnday() {
+        return earnday;
     }
-    
-     public byte[]  getbanksignature()
-    {
-        return  banksignature;
+
+    public byte[] getbanksignature() {
+        return bankSignature;
     }
-    
-    public byte[]  getdrawersiganure()
-    {
-        return  drawersiganure;
+
+    public byte[] getdrawersiganure() {
+        return drawerSignature;
     }
 
     public void saveCheque(String filename) throws IOException {
@@ -176,5 +151,5 @@ public class ECheque implements Serializable
         in.close();
         return cheq;
     }
-   
+
 }
