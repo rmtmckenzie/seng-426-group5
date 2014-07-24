95b0f652d68d8c46e7d9430e701ea24ed09adb92
Logan Bissonnette <lbiss@uvic.ca>
2014-07-24 00:12:36 -0700

    Fixes and removal of dead code in DigitalCertificate

--- The e-Cheque Client/src/eCheque/DigitalSignature.java
+++ The e-Cheque Client/src/eCheque/DigitalSignature.java
@@ -19,23 +19,22 @@ import java.security.PublicKey;
 import java.security.Signature;
 
 public class DigitalSignature {
-	public DigitalSignature() {
-	}
+    public DigitalSignature() {
+    }
 
-	// this function is used to sign cheque data by RSA algorithm
-	public byte[] signature(String message, PrivateKey privKey) throws Exception {
-		Signature signmessage = Signature.getInstance("SHA1withRSA");
-		signmessage.initSign(privKey);
-		signmessage.update(message.getBytes());
-		byte[] signature = signmessage.sign();
-		return signature;
-	}
+    // this function is used to sign cheque data by RSA algorithm
+    public byte[] signature(String message, PrivateKey privKey) throws Exception {
+        Signature signMessage = Signature.getInstance("SHA1withRSA");
+        signMessage.initSign(privKey);
+        signMessage.update(message.getBytes());
+        return signMessage.sign();
+    }
 
-	// this function is used to verify the signature to cheque data by RSA algorithm
-	public boolean verifySignature(byte[] messagesign, String message, PublicKey pubKey) throws Exception {
-		Signature verifyMessage = Signature.getInstance("SHA1withRSA");
-		verifyMessage.initVerify(pubKey);
-		verifyMessage.update(message.getBytes());
-		return verifyMessage.verify(messagesign);
-	}
+    // this function is used to verify the signature to cheque data by RSA algorithm
+    public boolean verifySignature(byte[] messageSign, String message, PublicKey pubKey) throws Exception {
+        Signature verifyMessage = Signature.getInstance("SHA1withRSA");
+        verifyMessage.initVerify(pubKey);
+        verifyMessage.update(message.getBytes());
+        return verifyMessage.verify(messageSign);
+    }
 }
