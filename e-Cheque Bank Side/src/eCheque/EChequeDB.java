8ad31f93530af2fc31e2fb45bd21eecbf63effe4
Logan Bissonnette <lbiss@uvic.ca>
2014-07-26 23:49:05 -0700

    BIG CHANGE: Deleted unused functions in echequeDB

--- e-Cheque Bank Side/src/eCheque/EChequeDB.java
+++ e-Cheque Bank Side/src/eCheque/EChequeDB.java
@@ -73,20 +73,6 @@ public class EChequeDB {
         }
     }
 
-    private boolean createStatement() throws SQLException {
-        sqlStatement = connection.createStatement();
-        return true;
-    }
-
-    private void executeSQLStatement(String statement, int statType) throws SQLException {
-        // Initialize sql statement and execute it.
-        if (statType == 0) {
-            resultSet = sqlStatement.executeQuery(statement);
-        } else if (statType == 1) {
-            sqlStatement.executeUpdate(statement);
-        }
-    }
-    
     public boolean runQuery(String statement, Object ... objects) {
         boolean result = false;
         
@@ -145,57 +131,5 @@ public class EChequeDB {
             }
         }
         return obj;
-    }
-
-    public boolean runDB(int databaseMode, String databaseStat) {
-        boolean flag = false;
-        try {
-            connectToDataBase();
-            createStatement();
-            executeSQLStatement(databaseStat, databaseMode);
-            flag = true;
-        } catch (ClassNotFoundException exp) {
-            exp.printStackTrace();
-        } catch (SQLException exp) {
-            exp.printStackTrace();
-        } finally {
-            closeDataBaseConnection();
-        }
-        return flag;
-    }
-
-    public boolean runDB(int databaseMode, String databaseStat, double[] balance) {
-        boolean flag = runDB(databaseStat, databaseMode);
-        try {
-            balance[0] = flag ? resultSet.getDouble(1) : 0.0;
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage(), "DB Error", JOptionPane.ERROR_MESSAGE);
-            e.printStackTrace();
-        }
-        return flag;
-    }
-
-    public boolean runDB(String databaseStat, int databaseMode) {
-        boolean flag = false;
-        try {
-            connectToDataBase();
-            JOptionPane.showMessageDialog(null, "You are connected to e-Cheque Bank DB", "DB State", JOptionPane.INFORMATION_MESSAGE);
-            createStatement();
-            JOptionPane.showMessageDialog(null, "You have created statement", "DB State", JOptionPane.INFORMATION_MESSAGE);
-
-            // run the specific sql statement
-            executeSQLStatement(databaseStat, databaseMode);
-            flag = resultSet.next();
-        } catch (ClassNotFoundException exp) {
-            JOptionPane.showMessageDialog(null, exp.getMessage(), "DB Error", JOptionPane.ERROR_MESSAGE);
-            exp.printStackTrace();
-
-        } catch (SQLException exp) {
-            JOptionPane.showMessageDialog(null, exp.getMessage(), "DB Error", JOptionPane.ERROR_MESSAGE);
-            exp.printStackTrace();
-        } finally {
-            closeDataBaseConnection();
-        }
-        return flag;
     }
 }
